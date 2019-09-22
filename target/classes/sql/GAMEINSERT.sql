9d309378ead520998ef4f87041f7f5e3843e25da
ppej1 <xodnjs1016@gmail.com>
2019-09-22 16:56:47 +0900

    0902 1656

---
+++ target/classes/sql/GAMEINSERT.sql
@@ -0,0 +1,35 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-8월-26-2019   
+--------------------------------------------------------
+REM INSERTING into CONSOLGAME
+SET DEFINE OFF;
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (1,'스파이로 트릴로지','인섬니악 게임즈','스위치 게임이다','fireemblem.png','액션 플랫포머','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (2,'토치라이트 2','Panic Button
+','스위치 게임이다','fireemblem.png','RPG
+','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (4,'열혈경파 쿠니오군 외전 리버시티 걸즈[정발]','웨이포워드','스위치 게임이다','fireemblem.png','액션','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (5,'DAEMON X MACHINA[독점][정발]','웨이포워드','스위치 게임이다','fireemblem.png','메카닉 액션','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (6,'사향의 라이라 ~Trap of MUSK~','마벨러스','스위치 게임이다','fireemblem.png','어드벤처','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (7,'AI: 솜니엄 파일즈','스파이크 춘소프트','스위치 게임이다','fireemblem.png','어드벤처','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (8,'니노쿠니 하얀 성회의 여왕','레벨파이브','스위치 게임이다','fireemblem.png','RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (9,'젤다의 전설 꿈꾸는 섬[독점][정발]','닌텐도','스위치 게임이다','fireemblem.png','액션 어드벤처','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (10,'콘트라 로그 코어','코나미','스위치 게임이다','fireemblem.png','슈팅 액션','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (11,'DEAD BY DAYLIGHT','비헤이비어 디지털','스위치 게임이다','fireemblem.png','공포','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (12,'Blasphemous','The Game Kitchen','스위치 게임이다','fireemblem.png','핵앤슬래시','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (13,'드래곤 퀘스트 XI S[정발]','스퀘어 에닉스','스위치 게임이다','g8.jpg','RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (14,'Ori and the Blind Forest Definitive Edition','액션 플랫포머','스위치 게임이다','g7.jpg','Moon Studio','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (15,'Ori and the Blind Forest Definitive Edition','반다이 남코','스위치 게임이다','g7.jpg','시뮬레이션 RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (16,'슈퍼로봇대전 V','반다이 남코','스위치 게임이다','g6.jpg','RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (17,'디지몬 스토리 사이버 슬루스 해커스 메모리[정발]','반다이 남코','스위치 게임이다','g5.jpg','RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (18,'A Hat in Time','Gears for Breakfast','스위치 게임이다','g4.jpg','플랫포머','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (19,'마계전기 디스가이아 4 리턴','니혼이치','스위치 게임이다','g3.jpg','사상최흉의 시뮬레이션 RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (20,'루이지 맨션 3[독점][정발]','넥스트 레벨 게임즈','스위치 게임이다','g2.jpg','호러 어드벤처','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (21,'마리오와 소닉 AT 2020 도쿄 올림픽[독점][정발]','세가','스위치 게임이다','g1.jpg','액션 스포츠','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (22,'파이어 엠블렘 풍화설월','인텔리전트 시스템','8월 최고의 게임 풍화설월','fireemblem.png','RPG','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (23,'요시크래프트 월드','굿-필(Good-Feel)','요시 크래프트다','yosi.jpg','액션','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (24,'마리오메이커','닌텐도','2019년 6월 28일 Nintendo Switch로 발매되는 슈퍼 마리오 메이커의 속편이다. 만들고, 플레이라는 장르명대로, 직접 코스를 만들고, 자신과 다른 사람이 만든 코스를 플레이할 수 있다. 이번 작품에선 스토리 모드와 로컬/온라인 멀티 플레이, 슈퍼 마리오 3D 월드 게임 배경이나 여러 파츠가 추가되는 등 많은 추가 요소가 있어 출시 전 부터 기대를 받았다.','mariomaker.png','샌드박스','switch');
+Insert into CONSOLGAME (GAMENUM,GAMETITLE,PRODUCTION,CONTENT,IMAGEURL,GENRE,TYPENAME) values (25,'스플래툰','닌텐도','닌텐도에서 개발한 TPS 게임이자, 스플래툰 시리즈의 첫작이다. 참으로 오랜만에 등장한 닌텐도 신규 IP로, 마리오 카트에 필적하는 마케팅 비용을 책정받을 만큼 회사가 신임하는 IP다. 일본에서는 순조로운 판매를 기록한다. 닌텐도의 핵심 IP군에 빠르게 정착하였으며, 슈퍼 스매시 브라더스 4에도 DLC 유저 아이템으로 참전했고 얼티밋에선 아예 신규캐로 추가되었다. 본 문서에선 각종 명칭을 일본어판을 기준으로 하고 영칭은 괄호 속 기울임꼴로 표기한다.
+닌텐도에서 개발한 TPS 게임이자, 스플래툰 시리즈의 첫작이다. 참으로 오랜만에 등장한 닌텐도 신규 IP로, 마리오 카트에 필적하는 마케팅 비용을 책정받을 만큼 회사가 신임하는 IP다. 일본에서는 순조로운 판매를 기록한다. 닌텐도의 핵심 IP군에 빠르게 정착하였으며, 슈퍼 스매시 브라더스 4에도 DLC 유저 아이템으로 참전했고 얼티밋에선 아예 신규캐로 추가되었다. 본 문서에선 각종 명칭을 일본어판을 기준으로 하고 영칭은 괄호 속 기울임꼴로 표기한다.
+
+닌텐도 스위치로 후속작 스플래툰 2가 발매되었다.
+닌텐도 스위치로 후속작 스플래툰 2가 발매되었다.','splatoon.jpg','TPS','switch');
